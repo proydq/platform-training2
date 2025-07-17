825b831c9a8480b392f726358f9807f98b5783ff
DarkDog2020 <851788565@qq.com>
2025-07-17 10:24:30 +0800

    首次提交

---
+++ backend/src/main/java/com/example/smarttrainingsystem/SmartTrainingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarttrainingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTrainingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTrainingSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/smarttrainingsystem/SmartTrainingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarttrainingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTrainingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
